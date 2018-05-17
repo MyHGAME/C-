0ac1539f8af3dd0cdb30f0965219fe7261feb86c
黄海庭 <290525448@qq.com>
2018-05-17 16:12:34 +0800

    Update 2-1.cpp

--- 2-1.cpp
+++ 2-1.cpp
@@ -1,15 +1,64 @@
-//Î´ÍêÉÆ£ºÒ»¸ö×Ö·û´®·ÖÀë³öÊı×ÖÓë·ûºÅ£¬Êı×ÖÒªÒ»´®»®·ÖÍêÕûµÄÊı×Ö
-//·ûºÅÓÅÏÈ¼¶µÄÎÊÌâ
+//æœªå®Œå–„ï¼šä¸€ä¸ªå­—ç¬¦ä¸²åˆ†ç¦»å‡ºæ•°å­—ä¸ç¬¦å·ï¼Œæ•°å­—è¦ä¸€ä¸²åˆ’åˆ†å®Œæ•´çš„æ•°å­—
+//ç¬¦å·ä¼˜å…ˆçº§çš„é—®é¢˜
 #include <iostream>
 #include <stack>
 #include <list>
 #include <string>
 #include <sstream>
+#include <cctype>
+#include <math.h>
+#include <stdlib.h>
 using namespace std;
 bool isnum(string);
 bool IsUp(string, string);
 list<string> Change(list<string>);
 double Operation(list<string>);
+list<string> InData(string s);
+list<string> InData(string s)
+{
+
+	list<string> thelist;
+	int n = 0;
+	while(n < s.size())
+	{
+		if(isdigit(s[n]))
+		{
+			int i = n;
+			float sum = 0;
+			while(isdigit(s[i]))
+			{
+				sum *=10;
+				sum = sum + (s[i]-48);
+				i++;
+			}
+			if(s[i] == '.')
+			{
+				int j = i+1,num = -1;
+				float sum2 = 0;
+				while(isdigit(s[j]))
+				{
+					sum2 += (s[j]-48)*pow(10,num);
+					num--;
+					j++;
+				}
+				i = j-1 ;
+				sum += sum2;
+			}
+			ostringstream oss;  //åˆ›å»ºä¸€ä¸ªæ ¼å¼åŒ–è¾“å‡ºæµ
+            oss<<sum;             //æŠŠå€¼ä¼ é€’å¦‚æµä¸­
+			thelist.push_back(oss.str());
+			n = i;
+		}
+		else
+		{
+            string str(1, s[n]);
+			thelist.push_back(str);
+		}
+		n++;
+	}
+	return thelist;
+}
+
 bool IsUp(string a, string b)
 {
 	if (b == "(")
@@ -35,7 +84,7 @@ bool isnum(string s)
 	else
 		return true;
 }
-//½«±í´ïÊ½×ª»»³Éºó×º±í´ïÊ½
+//å°†è¡¨è¾¾å¼è½¬æ¢æˆåç¼€è¡¨è¾¾å¼
 list<string> Change(list<string> A)
 {
 	int ArrLenth = 0;
@@ -92,7 +141,7 @@ double Operation(list<string> Expression)
 {
 	stack<double> result;
 	list<string>::iterator iter;
-	cout << endl << "¼ÆËã¹ı³Ì£º";
+	cout << endl << "è®¡ç®—è¿‡ç¨‹ï¼š";
 	try
 	{
 		for (iter = Expression.begin(); iter != Expression.end(); iter++)
@@ -143,7 +192,7 @@ double Operation(list<string> Expression)
 	{
 		if (e == 0)
 		{
-			cout << "³öÏÖ³ı 0 ´íÎó" << endl;
+			cout << "å‡ºç°é™¤ 0 é”™è¯¯" << endl;
 		}
 	}
 
@@ -151,45 +200,54 @@ double Operation(list<string> Expression)
 }
 int main()
 {
-	list<string> test;
-	test.push_back("(");
-	test.push_back("12");
-	test.push_back("*");
-	test.push_back("23");
-	test.push_back("*");
-	test.push_back("(");
-	test.push_back("31");
-	test.push_back("+");
-	test.push_back("63");
-	test.push_back("-");
-	test.push_back("43");
-	test.push_back("*");
-	test.push_back("(");
-	test.push_back("6");
-	test.push_back("+");
-	test.push_back("62");
-	test.push_back(")");
-	test.push_back(")");
-	test.push_back(")");
-	test.push_back("+");
-	test.push_back("6");
-	test.push_back("+");
-	test.push_back("62");
-	list<string> Expression = Change(test);
-	list<string>::iterator iter;
-	cout << "Ô­±í´ïÊ½£º" << endl;
-	for (iter = test.begin(); iter != test.end(); iter++)
+//	list<string> test;
+//	test.push_back("(");
+//	test.push_back("12");
+//	test.push_back("*");
+//	test.push_back("23");
+//	test.push_back("*");
+//	test.push_back("(");
+//	test.push_back("31");
+//	test.push_back("+");
+//	test.push_back("63");
+//	test.push_back("-");
+//	test.push_back("43");
+//	test.push_back("*");
+//	test.push_back("(");
+//	test.push_back("6");
+//	test.push_back("+");
+//	test.push_back("62");
+//	test.push_back(")");
+//	test.push_back(")");
+//	test.push_back(")");
+//	test.push_back("+");
+//	test.push_back("6");
+//	test.push_back("+");
+//	test.push_back("62");
+//	list<string> Expression = Change(test);
+//	list<string>::iterator iter;
+//	cout << "åŸè¡¨è¾¾å¼ï¼š" << endl;
+//	for (iter = test.begin(); iter != test.end(); iter++)
+//	{
+//		cout << " " << *iter;
+//	}
+//	cout << endl;
+//	cout << "åç¼€è¡¨è¾¾å¼ï¼š" << endl;
+//	for (iter = Expression.begin(); iter != Expression.end(); iter++)
+//	{
+//		cout << " " << *iter;
+//	}
+//	cout << endl << "ç»“æœï¼š" << Operation(Expression) << endl;
+	string s;
+	cin>>s;
+    list<string> data = InData(s);
+    data = Change(data);
+    list<string>::iterator iter2;
+	for(iter2 = data.begin();iter2 != data.end();iter2++)
 	{
-		cout << " " << *iter;
+		cout << " " << *iter2;
 	}
-	cout << endl;
-	cout << "ºó×º±í´ïÊ½£º" << endl;
-	for (iter = Expression.begin(); iter != Expression.end(); iter++)
-	{
-		cout << " " << *iter;
-	}
-	cout << endl << "½á¹û£º" << Operation(Expression) << endl;
+		cout << endl << "ç»“æœï¼š" << Operation(data) << endl;
 	system("pause");
 	return 0;
 }
-
